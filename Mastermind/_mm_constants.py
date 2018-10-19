71734cd1f50ebf09d3650b5686a27d736898ab5e
stolencatkarma <enjia2000@gmail.com>
2018-10-19 10:44:45 -0400

    whoops. had those backwards.

--- Mastermind/_mm_constants.py
+++ Mastermind/_mm_constants.py
@@ -6,6 +6,6 @@ MM_UNKNOWN = -1
 MM_MAX = -2 #Cannot be in [0,9] because these are already valid arguments for compression.
 
 # Maximum payload to attempt to extract from the stream. This serves as minor DOS protection
-MM_MAX_PAYLOAD_SIZE = 2048
+MM_MAX_PAYLOAD_SIZE = 65535
 # Minimum payload size to consider compression. Otherwise the payload may result in a net loss.
-MM_MIN_PAYLOAD_COMPRESSION_SIZE = 65535 # maximum size for a TCP packet
+MM_MIN_PAYLOAD_COMPRESSION_SIZE = 2048 # maximum size for a TCP packet
