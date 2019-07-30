769d405a6843d9b0c7f789b1f91da50bf414cfeb
David Brown <davidpwbrown@gmail.com>
2019-07-30 15:06:32 +0100

    used tilemap/tileset for operations to draw

--- GodotClient/window_main.gd
+++ GodotClient/window_main.gd
@@ -3,9 +3,7 @@ var _dt = preload("res://DynamicTile.tscn")
 
 func _process(delta):
 	if manager_connection.should_update_localmap:
-		var dynamic_tiles = get_tree().get_nodes_in_group("DynamicTiles")
-		for dt in dynamic_tiles:
-	    	dt.queue_free()
+		$terrain_tileset.clear()
 		var draw_x = null
 		var draw_y = null
 		# loop through the chunks finding smallest x and y for our origin draw point
@@ -21,12 +19,9 @@ func _process(delta):
 		# now we have a draw_x, and draw_y origin. loop through again and draw the tiles.
 		for chunk in manager_connection.localmap_chunks:
 			for tile in chunk['tiles']:
-				# draw a dynamic tile at that position and fill it with the data from the dictionary. 
-				var _instance = _dt.instance()
-				add_child(_instance)
-				_instance.position.x = (tile['position']['x'] - draw_x) * 32
-				_instance.position.y = (tile['position']['y'] - draw_y) * 32
-				_instance.add_to_group("DynamicTiles")
+				var tile_index = $terrain_tileset.get_tileset().find_tile_by_name("t_dirt")
+				var xy = Vector2( (tile['position']['x'] - draw_x), (tile['position']['y'] - draw_y) )
+				$terrain_tileset.set_cellv( xy, tile_index )
 				
 		# finally set back to false until we recieve a new localmap from the server
 		manager_connection.should_update_localmap = false
