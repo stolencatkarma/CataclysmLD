d22034304450b1f60a7cd07e0c32bc51980f5c19
stolencatkarma <enjia2000@gmail.com>
2019-10-18 13:22:40 -0400

    basic inventory display of slots and containers.

--- client/Scenes/Game_Main/Interface/Scripts/Inventory.gd
+++ client/Scenes/Game_Main/Interface/Scripts/Inventory.gd
@@ -2,10 +2,8 @@ extends Node2D
 
 # inventory screen uses the same tilemap as items.
 onready var items_tilemap = get_parent().get_parent().get_node("Viewport/node_window_main/items_tilemap")
+var container_slot = load('res://Scenes/Game_Main/Interface/container_slot.tscn')
 
-# vars for items in containers.
-var startx = 0
-var starty = 384
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,44 +12,47 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if self.visible and manager_connection.should_update_inventory:
-		$inventory_tilemap.clear()
 		# update inventory screen from latest update.
 		manager_connection.should_update_inventory = false
+		for child in $container_slots.get_children():
+			child.queue_free()
+		# vars for items in containers.
+		var startx = 0
+		var starty = 384
 		for body_part in manager_connection.controlled_character['body_parts']:
 			if(body_part['slot0'] != null):
-				# need dict and Texture
 				var _contained = body_part['slot0'].duplicate()
+
 				get_node(body_part['ident']).get_node('slot0').contained = _contained
-				# print(_contained)
+
 				var _tileset = items_tilemap.get_tileset()
 				var item_index = _tileset.find_tile_by_name(_contained["ident"])
-				
-				var slot_pos = get_node(body_part['ident']).get_node('slot0').position
-				
-				var xy = Vector2(slot_pos.x/32,slot_pos.y/32)
-				$inventory_tilemap.set_cellv( xy, item_index )
+				var region = items_tilemap.get_tileset().tile_get_region ( item_index )
+				var at = AtlasTexture.new()
+				at.set_atlas(items_tilemap.get_tileset().tile_get_texture( item_index ))
+				at.set_region(region)
+				get_node(body_part['ident']).get_node('slot0').button.icon = at
 				
 				if('opened' in body_part['slot0']):
 					if(body_part['slot0']['opened'] == 'yes'):
 						for item in body_part['slot0']['contained_items']:
-							print(item['ident'])
-							
-							if(startx > 352):
+							var _slot = container_slot.instance() 
+							var _contained_in = item.duplicate()
+							var at2 = AtlasTexture.new()
+							var item_index2 = _tileset.find_tile_by_name(item["ident"])
+							var region2 = items_tilemap.get_tileset().tile_get_region ( item_index2 )
+							at2.set_atlas(items_tilemap.get_tileset().tile_get_texture( item_index2 ))
+							at2.set_region(region2)
+							$container_slots.add_child(_slot)
+							_slot.contained = _contained_in
+							_slot.button.set_button_icon(at2)
+							if(startx > 320):
 								startx = 0
 								starty = starty + 32
 							startx = startx + 32
-							# _button.connect("button_up", self, "submit_character")
+							
+							_slot.position = Vector2(startx, starty)
+							
 
 			
-			if(body_part['slot1'] != null):
-				# need dict and Texture
-				var _contained = body_part['slot1'].duplicate()
-				get_node(body_part['ident']).get_node('slot1').contained = _contained
-				# print(_contained)
-				var _tileset = items_tilemap.get_tileset()
-				var item_index = _tileset.find_tile_by_name(_contained["ident"])
-				
-				var slot_pos = get_node(body_part['ident']).get_node('slot1').position
-				
-				var xy = Vector2(slot_pos.x/32,slot_pos.y/32)
-				$inventory_tilemap.set_cellv( xy, item_index )
+			
