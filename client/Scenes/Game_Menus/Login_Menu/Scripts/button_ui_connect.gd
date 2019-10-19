db3e324a846d9184ba6fdef077f7c6b0cd3d71f4
Rod99 <roddyrod99@gmail.com>
2019-10-19 09:59:01 -0500

    Use Absolute Locations, not relative.
    Changes the node paths to use absolute location.

--- client/Scenes/Game_Menus/Login_Menu/Scripts/button_ui_connect.gd
+++ client/Scenes/Game_Menus/Login_Menu/Scripts/button_ui_connect.gd
@@ -1,8 +1,8 @@
 extends Button
 
 func _pressed():
-	manager_connection.HOST = get_parent().get_parent().get_node("server_container/ip-port_container/txt_IP").text
-	manager_connection.PORT = get_parent().get_parent().get_node("server_container/ip-port_container/txt_PORT").text
-	manager_connection.username = get_parent().get_parent().get_node("name_container/txt_username").text
-	manager_connection.password = get_parent().get_parent().get_node("password_container/txt_password").text
+	manager_connection.HOST = get_node("/root/Node2D/Menu_Margin/Menu_Container/server_container/ip-port_container/txt_IP").text
+	manager_connection.PORT = get_node("/root/Node2D/Menu_Margin/Menu_Container/server_container/ip-port_container/txt_PORT").text
+	manager_connection.username = get_node("/root/Node2D/Menu_Margin/Menu_Container/name_container/txt_username").text
+	manager_connection.password = get_node("/root/Node2D/Menu_Margin/Menu_Container/password_container/txt_password").text
 	manager_connection.connect_to_server()
